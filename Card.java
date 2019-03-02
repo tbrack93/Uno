7ee4a99f0e939bde835d6e4f360bcb37cf4ba05d
Tim Brackfield <Cloud_193@hotmail.com>
2019-03-02 10:59:47 +0000

    Created initial card and deck classes' ''

---
+++ Card.java
@@ -0,0 +1,75 @@
+public class Card{
+
+  public enum Action {Skip, Draw2, Reverse};
+  public enum Wild {Wild, Wild_Draw4};
+  public enum Colour {Green, Yellow, Blue, Red};
+
+  private Colour colour;
+  private Wild wild;
+  private Action action;
+  private Integer number;
+
+  public Card(Colour colour, int number){
+    this.colour = colour;
+    this.number = number;
+  }
+
+  public Card(Colour colour, Action action){
+    this.colour = colour;
+    this.action = action;
+  }
+
+  public Card(Wild wild){
+    this.wild = wild;
+  }
+
+  public String getColour(){
+    if(this.colour != null){
+      return this.colour.name();
+    }
+    return "white";
+  }
+
+  public int getNumber(){
+    if(this.number != null){
+      return this.number;
+    }
+    return -10;
+  }
+
+  public String getAction(){
+    if(this.action != null){
+      return this.action.name();
+    }
+    return null;
+  }
+
+  public String getWild(){
+    if(this.wild != null){
+      return this.wild.name();
+    }
+    return null;
+  }
+
+  public boolean match(Card card){
+    return this.getColour() == card.getColour() || this.getNumber() == card.getNumber();
+  }
+
+  // @Override
+  // public int compareTo(Card other){
+  //   if(this.getColour().compareTo(other.getColour()) == 0){
+  //     return this.getNumber() - other.getNumber();
+  //   }
+  //   return this.getColour().compareTo(other.getColour());
+  // }
+
+  public String toString(){
+    if(this.getWild() != null){
+      return this.getWild();
+    }
+    if(this.getAction() == null){
+      return this.getColour() + " : " + this.getNumber();
+    }
+    return this.getColour() + " : " + this.getAction();
+  }
+}
---
+++ Deck.java
@@ -0,0 +1,65 @@
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Deck{
+
+  private LinkedList<Card> cards;
+
+  public Deck() {
+    Card[] deck = new Card[108]; //faster to add to an array
+    int counter = 0;
+
+    for(Card.Colour colour : Card.Colour.values()){
+      deck[counter] = new Card(colour, 0);
+      counter++;
+      for(int i = 1; i < 10; i++){
+        deck[counter] = new Card(colour, i);
+        deck[counter+1] = new Card(colour, i);
+        counter = counter + 2;
+      }
+      for(Card.Action action : Card.Action.values()){
+        deck[counter] = new Card(colour, action);
+        deck[counter + 1] = new Card(colour, action);
+        counter = counter + 2;
+      }
+    }
+
+    for(Card.Wild wild : Card.Wild.values()){
+      for(int l = 0; l < 4; l++){
+        deck[counter] = new Card(wild);
+        System.out.println(deck[counter].getAction());
+        counter++;
+      }
+    }
+    cards = new LinkedList(Arrays.asList(deck));
+    Collections.shuffle(cards);
+    Collections.shuffle(cards);
+    Collections.shuffle(cards);
+  }
+
+  public LinkedList<Card> draw(int number){
+    LinkedList<Card> draw = new LinkedList<>();
+    for(int i = 0; i < number; i++){
+      if(cards.size() > 0){
+        draw.add(this.cards.remove());
+      }
+    }
+    Comparator<Card> comp = Comparator.comparing(Card::getColour).thenComparing(Card::getNumber);
+    Collections.sort(draw, comp);
+    return draw;
+  }
+
+  public static void main(String[] args){
+    Deck deck = new Deck();
+    // for(Card c : deck.cards){
+    //   System.out.println(c);
+    // }
+    System.out.println(deck.cards.size());
+    System.out.println(deck.draw(12));
+    System.out.println(deck.cards.size());
+
+  }
+}
